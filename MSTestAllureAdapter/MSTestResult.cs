8bcd2e8e1191047f10fcb92a2739c1d199b279a3
Daniel Kugel <mail4dk@gmail.com>
2014-09-15 19:47:36 +0300

    Added support for multiple test suite names.

--- MSTestAllureAdapter/MSTestResult.cs
+++ MSTestAllureAdapter/MSTestResult.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MSTestAllureAdapter
 {
@@ -6,16 +7,26 @@ namespace MSTestAllureAdapter
 
 	public class MSTestResult
 	{
-		public MSTestResult (string testName, string suiteName, TestOutcome outcome)
-		{
+		public MSTestResult(string testName, TestOutcome outcome, params string[] suiteNames) 
+		{ 
 			Name = testName;
-			Suite = suiteName;
+
+			// strings are immutable to no deep copy is needed.
+			string[] suiteNamesCopy = new string[suiteNames.Length];
+			Array.Copy(suiteNames, suiteNamesCopy, suiteNames.Length);
+
+			Suites = suiteNamesCopy;
+
 			Outcome = outcome;
 		}
 
+
+		public MSTestResult (string testName, TestOutcome outcome, string suiteName)
+			: this(testName, outcome, new string[] { suiteName }) { }
+
 		public string Name { get; private set; }
 
-		public string Suite { get; private set; }
+		public IEnumerable<string> Suites { get; private set; }
 
 		public TestOutcome Outcome { get; private set; }
 	}
