0cb76baf7bcbb735d8b87ab049197fd75f8bf7ef
Daniel Kugel <mail4dk@gmail.com>
2014-09-18 09:48:01 +0300

    Added MSTestAllureAdapter.Console project.

---
+++ MSTestAllureAdapter.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace MSTestAllureAdapter.Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string resultsDir = "results";
+
+            if (!Directory.Exists(resultsDir))
+            {
+                Directory.CreateDirectory(resultsDir);
+            }
+
+            if (!resultsDir.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                resultsDir += Path.DirectorySeparatorChar;
+            }
+
+            AllureAdapter adapter = new AllureAdapter();
+            adapter.Run("sample.trx", resultsDir);
+        }
+    }
+}
