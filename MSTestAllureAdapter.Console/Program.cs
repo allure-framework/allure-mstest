6c3b4744735fff3dadb088584b6448e6e72fa828
Daniel Kugel <mail4dk@gmail.com>
2014-10-02 09:43:20 +0300

    * Program.cs: Added command line argument parsing.
    * MSTestAllureAdapter.Console.csproj: Passed command line arguments.

--- MSTestAllureAdapter.Console/Program.cs
+++ MSTestAllureAdapter.Console/Program.cs
@@ -5,22 +5,81 @@ namespace MSTestAllureAdapter.Console
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            string resultsDir = "results";
+            string resultsDir;
+            string trxPath;
 
-            if (!Directory.Exists(resultsDir))
+            if (!ParseCommandLineOptions(args, out trxPath, out resultsDir))
             {
-                Directory.CreateDirectory(resultsDir);
+                DisplayHelp();
+                return 1;
             }
 
-            if (!resultsDir.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            if (!File.Exists(trxPath))
             {
-                resultsDir += Path.DirectorySeparatorChar;
+                System.Console.WriteLine("Unable to open file supplied TRX file: '" + trxPath + "'");
+                return 1;
             }
 
             AllureAdapter adapter = new AllureAdapter();
-            adapter.Run("sample.trx", resultsDir);
+            adapter.GenerateTestResults(trxPath, resultsDir);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Parses the command line options.
+        /// Because the there are only 2 options the parsing is done manually without a library.
+        /// </summary>
+        /// <returns><c>true</c>, if command line options were parsed, <c>false</c> otherwise.</returns>
+        /// <param name="args">The command line arguments.</param>
+        /// <param name="trxPath">Trx path.</param>
+        /// <param name="reportPath">Report path.</param>
+        private static bool ParseCommandLineOptions(string[] args, out string trxPath, out string outputPath)
+        {
+            outputPath = null;
+            trxPath = null;
+
+            if (args == null || args.Length == 0)
+                return false;
+
+            trxPath = args[0];
+
+            outputPath = args.Length > 2 ? args[1] : "results";
+
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            if (!outputPath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                outputPath += Path.DirectorySeparatorChar;
+            }
+
+            return true;
+        }
+
+        private static void DisplayHelp()
+        {
+            // if GetCommandLineArgs cannot obtain the program name then the first element of the array is
+            // string.Empty but the array size will always be greater than zero.
+            // Path.GetFileName handles string.Empty correctly.
+
+            string programName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+
+            string help = String.Empty;
+
+            help += "Usage: " + programName + " ";
+
+            string reportDirDisplayName = "<output target dir>";
+
+            help += "<TRX file> " + reportDirDisplayName;
+            help += Environment.NewLine;
+            help += "If " + reportDirDisplayName + " is missing the current directory is used.";
+
+            System.Console.WriteLine(help);
         }
     }
 }
