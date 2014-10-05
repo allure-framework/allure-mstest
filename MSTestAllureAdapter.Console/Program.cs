c5da3c9329e4bf95633f639883cdafa8e2ffdc98
Daniel Kugel <mail4dk@gmail.com>
2014-10-05 15:33:45 +0300

    Console return codes tests were added.
    Added tests to the console return codes.
    Added some error handeling and output.

--- MSTestAllureAdapter.Console/Program.cs
+++ MSTestAllureAdapter.Console/Program.cs
@@ -3,8 +3,12 @@ using System.IO;
 
 namespace MSTestAllureAdapter.Console
 {
-    class MainClass
+    internal class MainClass
     {
+        const int ERROR = 1;
+        const int OK = 0;
+        const string DEFAULT_RESULT_DIR = "results";
+
         public static int Main(string[] args)
         {
             string resultsDir;
@@ -13,24 +17,34 @@ namespace MSTestAllureAdapter.Console
             if (!ParseCommandLineOptions(args, out trxPath, out resultsDir))
             {
                 DisplayHelp();
-                return 1;
+                return ERROR;
             }
 
             if (!File.Exists(trxPath))
             {
-                System.Console.WriteLine("Unable to open file supplied TRX file: '" + trxPath + "'");
-                return 1;
+                System.Console.WriteLine("The supplied TRX file: '" + trxPath + "' was not found.");
+                return ERROR;
             }
 
-            AllureAdapter adapter = new AllureAdapter();
-            adapter.GenerateTestResults(trxPath, resultsDir);
-
-            return 0;
+            try {
+                AllureAdapter adapter = new AllureAdapter();
+                System.Console.Write("Generating allure files... ");
+                adapter.GenerateTestResults(trxPath, resultsDir);
+                System.Console.WriteLine("Done.");
+                return OK;
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine("There was an error generating allure files into '" + resultsDir + "' from the TRX file '" + trxPath + "'.");
+                // perhaps in the future we'll use log4net.
+                System.Console.WriteLine(e.ToString());
+                return ERROR;
+            }
         }
 
         /// <summary>
         /// Parses the command line options.
-        /// Because the there are only 2 options the parsing is done manually without a library.
+        /// Because the there are only 2 options the parsing is done manually instead of using a library.
         /// </summary>
         /// <returns><c>true</c>, if command line options were parsed, <c>false</c> otherwise.</returns>
         /// <param name="args">The command line arguments.</param>
@@ -46,7 +60,7 @@ namespace MSTestAllureAdapter.Console
 
             trxPath = args[0];
 
-            outputPath = args.Length > 2 ? args[1] : "results";
+            outputPath = args.Length > 2 ? args[1] : DEFAULT_RESULT_DIR;
 
             if (!Directory.Exists(outputPath))
             {
@@ -73,11 +87,11 @@ namespace MSTestAllureAdapter.Console
 
             help += "Usage: " + programName + " ";
 
-            string reportDirDisplayName = "<output target dir>";
+            string targetDirDisplayName = "[output target dir]";
 
-            help += "<TRX file> " + reportDirDisplayName;
+            help += "<TRX file> " + targetDirDisplayName;
             help += Environment.NewLine;
-            help += "If " + reportDirDisplayName + " is missing the current directory is used.";
+            help += "If '" + targetDirDisplayName + "' is missing the reslts are saved in the current directory in a folder named '" + DEFAULT_RESULT_DIR  + "'.";
 
             System.Console.WriteLine(help);
         }
---
+++ MSTestAllureAdapter.Tests/ConsoleTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MSTestAllureAdapter.Console;
+using NUnit.Framework;
+
+namespace MSTestAllureAdapter.Tests
+{
+    [TestFixture]
+    public class ConsoleTests
+    {
+        [Test]
+        public void Missing_Arguments_Return_Error()
+        {
+            int expected = 1;
+            int actual = MainClass.Main(new string[0]);
+
+            Assert.AreEqual(expected, actual, "Main did not return the correct error code when no arguments were passed.");
+        }
+
+        [Test]
+        public void Missing_Trx_Returns_Error()
+        {
+            int expected = 1;
+            int actual = MainClass.Main(new string[]{ "non-existing-trx-file.trx" });
+
+            Assert.AreEqual(expected, actual, "Main did not return the correct error code on a missing TRX file.");
+        }
+
+        [Test]
+        public void Invalid_Trx_Returns_Error()
+        {
+            int expexted = 1;
+            int actual = MainClass.Main(new string[]{ "InvalidFile.trx" });
+
+            Assert.AreEqual(expexted, actual);
+        }
+
+        [Test]
+        public void Valid_Trx_Returns_OK()
+        {
+            int expexted = 0;
+            int actual = MainClass.Main(new string[]{ "sample.trx" });
+
+            Assert.AreEqual(expexted, actual);
+        }
+    }
+}
+
